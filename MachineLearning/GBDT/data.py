18a214d2686b46eeae207b2ef19043ac97e55741
charlesXu86 <charlesxu86@163.com>
2017-12-19 10:37:57 +0800

    GBDT数据预处理

---
+++ MachineLearning/GBDT/data.py
@@ -0,0 +1,21 @@
+#-*- coding:utf-8 _*-  
+""" 
+@author:charlesXu
+@file: data.py 
+@desc: GBDT数据预处理
+@time: 2017/12/19 
+"""
+
+class DataSet:
+    '''
+    分类问题默认标签列名称为label，二元分类标签∈{-1, +1}
+    回归问题也统一使用label
+    '''
+    def __init__(self, filename):
+        line_cnt = 0
+        self.instances = dict()
+        self.distinct_valueset = dict()     # just for real value type
+        for line in open(filename):
+            if line == '\n':
+                continue
+            fileds = line[:-1].split(',')
