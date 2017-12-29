aa71fda996566beded1e10114a8c8d8a391f6eff
charlesXu86 <charlesxu86@163.com>
2017-12-29 16:04:41 +0800

    SparkSQl的各种数据源文件读取

--- PythonSpark/SparkSQL/DataSource.py
+++ PythonSpark/SparkSQL/DataSource.py
@@ -14,14 +14,30 @@ from pyspark.sql import Row
 
 def basic_datasource_example(spark):
     df = spark.read.load('F:\project\MachineLearning\PythonSpark\Data\\users.parquet')
-    df.select("name", "favorate_color").write.save("namesAndFavCOlors.parquet")
+    df.select("name", "favorate_color").write.save("namesAndFavColors.parquet")
+    df.write.partitionBy("favorite_color").format("paequet").save("namesPartByColor.parquet")
 
+    df = spark.read.parquet('F:\project\MachineLearning\PythonSpark\Data\\users.parquet')
+    (df
+            .write
+            .partitionBy("favorite_color")
+            .bucketBy(42, "name")
+            .saveAsTable("people_partitioned_bucketed"))
 
+    # Manual_load_options
+    df = spark.read.load('F:\project\MachineLearning\PythonSpark\Data\people.json', format="json")
+    df.select("name", "age").write.save("nameAndAges.parquet", format="parquet")
+
+    df.write.bucketBy(42, "name").sortBy("age").saveAsTable("people_bucketed")
+
+    df = spark.sql("SELECT FROM parquet. 'F:\project\MachineLearning\PythonSpark\Data\\users.parquet'")
+    spark.sql("DROP TABLE IF EXISTS people_bucketed")
+    spark.sql("DROP TABLE IF EXISTS people_partitioned_bucketed")
 
 if __name__ == '__main__':
     spark = SparkSession.builder\
                         .appName("Python Spark SQL datasource Example")\
-                        .getOrCreate
+                        .getOrCreate()
     basic_datasource_example(spark)
     spark.stop()
 
