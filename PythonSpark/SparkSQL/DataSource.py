3850714bcc5d8137cd4f63d2518d74cfafd33f59
charlesXu86 <charlesxu86@163.com>
2017-12-29 14:19:45 +0800

    SparkSQl的各种数据源文件读取

---
+++ PythonSpark/SparkSQL/DataSource.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 _*-  
+""" 
+@author:charlesXu
+@file: DataSource.py 
+@desc: SparkSQl的各种数据源文件读取
+@time: 2017/12/29 
+"""
+
+from __future__ import print_function
+
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+
