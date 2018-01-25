5ff9031e4f40a5c91d3d58dfcf30049f2b25253d
charlesXu86 <charlesxu86@163.com>
2018-01-25 14:56:28 +0800

    添加pandas笔记

--- MachineLearning/Pandas/learnPandas.py
+++ MachineLearning/Pandas/learnPandas.py
@@ -5,4 +5,12 @@
 @desc:
       中文文档 ：http://python.usyiyi.cn/translate/Pandas_0j2/index.html
 @time: 2017/10/06 
-"""  
+"""
+
+import numpy as np
+import pandas as pd
+
+# 1,pd.read_csv()  读取csv(逗号分隔)文件到DatFrame
+#                 具体说明参考博客: https://www.cnblogs.com/datablog/p/6127000.html
+#                   参数说明: sep: str, default ',' 指定分隔符，如果不指定参数，则会尝试使用逗号分隔
+#                           delimiter: 定界符,备选分隔符(如果指定该参数，则sep参数失效)
