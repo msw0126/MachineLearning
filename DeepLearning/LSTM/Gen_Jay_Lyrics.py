e37c590d67fbf1fb6211743f8d1139d72a2bd44a
charlesXu86 <charlesxu86@163.com>
2018-01-26 13:16:05 +0800

    生成周杰伦歌词

---
+++ DeepLearning/LSTM/Gen_Jay_Lyrics.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 _*-  
+""" 
+@author:charlesXu
+@file: Gen_Jay_Lyrics.py 
+@desc: 生成周杰伦歌词
+      参考博客: http://leix.me/2016/11/28/tensorflow-lyrics-generation/
+@time: 2018/01/26 
+"""  
