2ed823af90b126989f1ee48b004c1fb8998990b8
charlesXu86 <charlesxu86@163.com>
2018-02-01 08:47:38 +0800

    leetcode第九题

---
+++ MachineLearning/Leetcode/009.palindrome-number/Palindrome_Number.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: Palindrome_Number.py
+@time: 2018/1/31 8:47
+@desc: 回文数
+"""
+
+if __name__ == '__main__':
+    pass
