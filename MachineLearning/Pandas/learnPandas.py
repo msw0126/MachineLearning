741c337e1ecfd5f2d6d31fd723afc2d8ddc55b88
charlesXu86 <charlesxu86@163.com>
2018-02-02 09:33:09 +0800

    pandas学习笔记

--- MachineLearning/Pandas/learnPandas.py
+++ MachineLearning/Pandas/learnPandas.py
@@ -20,8 +20,13 @@ import pandas as pd
 # 2, pd.get_dummies(data, prefix=None, prefix_sep='_', dummy_na=False, columns=None, sparse=False, drop_first=False)
 #       将分类变量转换为虚拟/指示符变量
 # 参数说明: data: array-like，Series或DataFrame
-x = pd.Series(list('abcdb'))
-df = pd.DataFrame({'A': ['a','b','c'], 'B': ['b','a','c'], 'C':[1,2,3]})
-y = pd.get_dummies(x)
-y1 = pd.get_dummies(df, prefix=['col1', 'col2'])
-print(y1)
+# x = pd.Series(list('abcdb'))
+# df = pd.DataFrame({'A': ['a','b','c'], 'B': ['b','a','c'], 'C':[1,2,3]})
+# y = pd.get_dummies(x)
+# y1 = pd.get_dummies(df, prefix=['col1', 'col2'])
+# print(y1)
+
+# pd.to_datetime(*args, **kwargs)  将参数转换为datetime
+df = pd.DataFrame({'year':[2015, 2016], 'month':[2,4], 'day':[4,5]})
+x = pd.to_datetime(df.split())
+print(x)
