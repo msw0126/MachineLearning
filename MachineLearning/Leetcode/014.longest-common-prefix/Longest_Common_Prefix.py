9aaa4bad3a00f69e8d01c1b3ff347c2c2221cf52
charlesXu86 <charlesxu86@163.com>
2018-02-03 17:28:28 +0800

    leetcode第14题

---
+++ MachineLearning/Leetcode/014.longest-common-prefix/Longest_Common_Prefix.py
@@ -0,0 +1,30 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: Longest_Common_Prefix.py
+@time: 2018/2/3 11:34
+@desc: 寻找数组最大公共前缀
+"""
+
+'''
+  解析:给定一个string类型数组，要求写一个方法，返回数组中这些字符串的最长公共前缀。
+  
+  思路:
+     将str[0]当做临时最长公共前缀，然后用这个前缀依次和剩下的字符串比较，输出最大公共字符串
+'''
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        comPre = str[0]
+
+
+
+if __name__ == '__main__':
+    pass
