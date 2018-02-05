1135a498edde068b15c5a7e7cfd83671175cc79b
charlesXu86 <charlesxu86@163.com>
2018-02-05 11:10:32 +0800

    leetcode第28题

---
+++ MachineLearning/Leetcode/028.implement-strStr()/Implement_strStr.py
@@ -0,0 +1,32 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: Implement_strStr.py
+@time: 2018/2/5 10:40
+@desc:
+"""
+
+'''
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+'''
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+
+if __name__ == '__main__':
+    pass
