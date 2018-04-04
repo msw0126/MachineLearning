52d8efe5ca7d15f3be13101d55be52345ff3d158
charlesXu86 <charlesxu86@163.com>
2018-04-04 17:54:14 +0800

    BiLSTM + CRF 模型层

--- DeepLearning/BILSTM-CRF/Model.py
+++ DeepLearning/BILSTM-CRF/Model.py
@@ -11,6 +11,7 @@
 
 import numpy as np
 import os, time, sys
+import tensorflow as tf
 
 from tensorflow.contrib.rnn import LSTMCell
 from tensorflow.contrib.crf import crf_log_likelihood
@@ -19,6 +20,63 @@ from tensorflow.contrib.crf import viterbi_decode
 from Data import pad_sequences, batch_yield
 from Utils import get_logger
 from Eval import conlleval
+
+class BiLSTM_CRF(object):
+    def __init__(self, args, embeddings, tag2label, vocab, paths, config):
+        self.batch_size = args.batch_size
+        self.epoch_num = args.epoch
+        self.hidden_dim = args.hidden_dim
+        self.embeddings = embeddings
+        self.CRF = args.CRF
+        self.update_embedding = args.update_embedding
+        self.dropout_keep_prob = args.dropout
+        self.optimizer = args.optimizer
+        self.lr = args.lr
+        self.clip_grad = args.clip
+        self.tag2label = tag2label
+        self.num_tags = len(tag2label)
+        self.vocab = vocab
+        self.shuffle = args.shuffle
+        self.model_path = paths['model_path']
+        self.summary_path = paths['summary_path']
+        self.logger = get_logger(paths['log_path'])
+        self.result_path = paths['result_path']
+        self.config = config
+
+    def build_graph(self):
+        self.add_placeholders()
+        self.lookup_layer_op()
+        self.biLSTM_layer_op()
+        self.softmax_pred_op()
+        self.loss_op()
+        self.trainstep_op()
+        self.init_op()
+
+    # 各层网络实现
+    def add_placeholders(self):
+        self.word_ids = tf.placeholder(tf.int32, shape=[None, None], name="word_ids")
+        self.labels = tf.placeholder(tf.int32, shape=[None, None], name="labels")
+        self.sequence_lengths = tf.placeholder(tf.int32, shape=[None], name="sequence_lengths")
+
+        # drop_out的作用
+        self.dropout_pl = tf.placeholder(dtype=tf.float32, shape=[], name="dropout")
+        # lr 的作用
+        self.lr_pl = tf.placeholder(dtype=tf.float32, shape=[], name="lr")
+
+    # 仔细理解
+    def lookup_layer_op(self):
+        with tf.variable_scope("words"):
+            # embedding 的作用
+            _word_embeddings = tf.Variable(self.embeddings, dtype=tf.float32, trainable=self.update_embedding,
+                                           name="_word_embeddings")
+            word_embeddings = tf.nn.embedding_lookup(params=_word_embeddings, ids=self.word_ids,
+                                                     name="word_embeddings")
+        self.word_embeddings = tf.nn.dropout(word_embeddings, self.dropout_pl)
+
+    def biLSTM_layer_op(self):
+        pass
+
+
 
 if __name__ == '__main__':
     pass
