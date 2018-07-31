e44c087a7cd890a706712d0cda43a32fdb17130b
charlesXu86 <charlesxu86@163.com>
2018-07-31 18:25:28 +0800

    tensorflow有关crf的学习笔记

--- DeepLearning/learnTF/Tf_CRF.py
+++ DeepLearning/learnTF/Tf_CRF.py
@@ -11,3 +11,21 @@
                    2018/7/31:
 -------------------------------------------------
 """
+
+import numpy as np
+import tensorflow as tf
+
+#data settings
+num_examples = 10
+num_words = 20
+num_features = 100
+num_tags = 5
+
+# 5 tags
+# random features
+x = np.random.rand(num_examples, num_words, num_features).astype(np.float32)
+
+y = np.random.randint(num_tags, size=[num_examples, num_words]).astype(np.int32)
+
+# 序列的长度
+sequence_lenths = np.full()
